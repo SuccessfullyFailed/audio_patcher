79bc0f4d26443764394204929cc4ff874f8914a4
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-03-09 19:53:28 +0100

    Let soundboard generator have owned buffer instead of static

--- src/audio_generators/sound_board.rs
+++ src/audio_generators/sound_board.rs
@@ -1,18 +1,15 @@
-use std::{ error::Error, sync::{ Mutex, MutexGuard }, thread::{ self, JoinHandle, sleep }, time::Duration };
+use std::{ error::Error, sync::{ Arc, Mutex, MutexGuard }, thread::{ self, JoinHandle, sleep }, time::Duration };
 use file_ref::{ DirMonitor, FileRef };
 use audio_buffer::AudioBuffer;
 use crate::AudioGenerator;
 
 
 
-static SOUNDBOARD_BUFFERS:Mutex<Vec<(Vec<f32>, usize, bool)>> = Mutex::new(Vec::new());
-
-
-
 pub struct SoundBoard<const SAMPLE_RATE:u32> {
 	source_dir:FileRef,
 	listener_thread:Option<JoinHandle<()>>,
-	buffer_index:usize
+	buffers:Arc<Mutex<Vec<(Vec<f32>, usize)>>>,
+	enabled:bool
 }
 impl<const SAMPLE_RATE:u32> SoundBoard<SAMPLE_RATE> {
 	
@@ -21,16 +18,13 @@ impl<const SAMPLE_RATE:u32> SoundBoard<SAMPLE_RATE> {
 		SoundBoard {
 			source_dir: FileRef::new(source_dir),
 			listener_thread: None,
-			buffer_index: {
-				let mut buffers_handle:MutexGuard<'_, Vec<(Vec<f32>, usize, bool)>> = SOUNDBOARD_BUFFERS.lock().unwrap();
-				buffers_handle.push((Vec::new(), 0, false));
-				buffers_handle.len() - 1
-			}
+			buffers: Arc::new(Mutex::new(Vec::new())),
+			enabled: false
 		}
 	}
 
 	/// Add a file to the static queue at the given buffer index.
-	fn add_file_to_queue(file:&FileRef, buffer_index:usize) {
+	fn add_file_to_queue(file:&FileRef, buffers:&Arc<Mutex<Vec<(Vec<f32>, usize)>>>) {
 
 		// Wait for file to be usable.
 		const INTERVAL:Duration = Duration::from_millis(10);
@@ -48,14 +42,12 @@ impl<const SAMPLE_RATE:u32> SoundBoard<SAMPLE_RATE> {
 		
 		
 		// Read the file and push it to the buffer.
-		if SOUNDBOARD_BUFFERS.lock().unwrap()[buffer_index].2 {
-			match AudioBuffer::from_wav(file.path()) {
-				Ok(mut buffer_from_file) => {
-					buffer_from_file.resample(2, SAMPLE_RATE);
-					SOUNDBOARD_BUFFERS.lock().unwrap()[buffer_index].0.extend_from_slice(buffer_from_file.data());
-				},
-				Err(error) => eprintln!("Could not play audio through soundboard: {error}")
-			}
+		match AudioBuffer::from_wav(file.path()) {
+			Ok(mut buffer_from_file) => {
+				buffer_from_file.resample(2, SAMPLE_RATE);
+				buffers.lock().unwrap().push((buffer_from_file.data().to_vec(), 0));
+			},
+			Err(error) => eprintln!("Could not play audio through soundboard: {error}")
 		}
 
 		// Delete file and return success.
@@ -79,11 +71,11 @@ impl<const SAMPLE_RATE:u32> AudioGenerator for SoundBoard<SAMPLE_RATE> {
 
 		// Start listener thread.
 		let source_dir:FileRef = self.source_dir.clone();
-		let buffer_index:usize = self.buffer_index;
+		let monitor_buffers:Arc<Mutex<Vec<(Vec<f32>, usize)>>> = Arc::clone(&self.buffers);
 		if self.listener_thread.is_none() {
 			self.listener_thread = Some(
 				thread::spawn(move || {
-					let monitor_launch:Result<(), Box<dyn Error>> = DirMonitor::new(source_dir.path()).with_add_handler(move |file| Self::add_file_to_queue(file, buffer_index)).run();
+					let monitor_launch:Result<(), Box<dyn Error>> = DirMonitor::new(source_dir.path()).with_add_handler(move |file| Self::add_file_to_queue(file, &monitor_buffers)).run();
 					if let Err(error) = monitor_launch {
 						eprintln!("SoundBoard could not launch DirMonitor: {error}");
 					}
@@ -91,47 +83,69 @@ impl<const SAMPLE_RATE:u32> AudioGenerator for SoundBoard<SAMPLE_RATE> {
 			);
 		}
 
-		// Set buffer as active.
-		SOUNDBOARD_BUFFERS.lock().unwrap()[self.buffer_index].2 = true;
-
 		// Return success.
+		self.enabled = true;
 		Ok(())
 	}
 
 	/// Stop the generator.
 	fn stop(&mut self) -> Result<(), Box<dyn Error>> {
-		self.listener_thread = None;
-		SOUNDBOARD_BUFFERS.lock().unwrap()[self.buffer_index] = (Vec::new(), 0, false);
+		self.enabled = false;
 		Ok(())
 	}
 
 	/// The amount of data currently available from the generator.
 	fn amount_available(&self) -> usize {
-		let (data, cursor, enabled) = &mut SOUNDBOARD_BUFFERS.lock().unwrap()[self.buffer_index];
-		if *enabled {
-			data.len() - *cursor
-		} else {
+		// If any buffers are still in use, return a very high amount available.
+		// This makes sure no trails of audio are left unplayed until the next buffer is added.
+		if self.enabled && self.buffers.lock().unwrap().is_empty() {
 			0
+		} else {
+			SAMPLE_RATE as usize
 		}
 	}
 
 	/// Try to take an amount of data from the buffer.
 	/// Returns None if the buffer does not contain enough data.
 	fn take(&self, amount:usize) -> Option<Vec<f32>> {
-		let (data, cursor, enabled) = &mut SOUNDBOARD_BUFFERS.lock().unwrap()[self.buffer_index];
-		let available:usize = data.len() - *cursor;
-		if !*enabled || available == 0  {
+		if !self.enabled {
+			return None;
+		}
+		let mut buffer_handle:MutexGuard<'_, Vec<(Vec<f32>, usize)>> = self.buffers.lock().unwrap();
+		if buffer_handle.is_empty() {
+			return None;
+		}
+
+		// Get buffer for each sound in the buffer.
+		let mut output_buffers:Vec<Vec<f32>> = Vec::with_capacity(buffer_handle.len());
+		let mut buffer_index:usize = 0;
+		while buffer_index < buffer_handle.len() {
+			let (buffer_data, cursor) = &mut buffer_handle[buffer_index];
+			let amount_available:usize = amount.min(buffer_data.len() - *cursor);
+			output_buffers.push(buffer_data[*cursor..*cursor + amount_available].to_vec());
+			*cursor += amount_available;
+
+			if *cursor < buffer_data.len() {
+				buffer_index += 1;
+			} else {
+				buffer_handle.remove(buffer_index);
+			}
+		}
+
+		// Combine the buffers into one output buffer.
+		if output_buffers.is_empty() {
 			None
-		} else if available > amount {
-			let output:Vec<f32> = data[*cursor..*cursor + amount].to_vec();
-			*cursor += amount;
-			Some(output)
 		} else {
-			let mut output:Vec<f32> = data[*cursor..*cursor + amount].to_vec();
-			output.extend(vec![0.0; amount - available]);
-			*data = Vec::new();
-			*cursor = 0;
-			Some(output)
+			let mut combined_buffer:Vec<f32> = output_buffers.remove(output_buffers.len() - 1);
+			if combined_buffer.len() < amount {
+				combined_buffer.extend(vec![0.0; amount - combined_buffer.len()]);
+			}
+			for additional_buffer in output_buffers {
+				for sample_index in 0..additional_buffer.len() {
+					combined_buffer[sample_index] += additional_buffer[sample_index];
+				}
+			}
+			Some(combined_buffer)
 		}
 	}
 }
